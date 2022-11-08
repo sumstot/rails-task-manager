644098b0ab55261f05cdc9a453d3a9cf1c073ff9
Soren Umstot <soren.umstot@gmail.com>
2022-11-08 16:08:47 +0900

    finished task manager needs formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Read all
+  get '/tasks', to: 'tasks#index', as: :tasks
+  # Create method
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+  # Read onemethod
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  # Update Method
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/task/:id', to: 'tasks#update'
+  # Destroy Method
+  delete '/tasks/:id', to: 'tasks#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
